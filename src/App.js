0ce1b97c9a019b6cc6d1516baafc5d2d92269d21
Mehmet Teker <teker.mehmed@gmail.com>
2020-05-01 12:02:09 +0300

    componentWillReceiveProps metotu test edildi.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Child from './Child';
 
 class App extends Component {
    
+  state = {
+    name:"Mehmet"
+  };
+
+
   constructor(props){
     super(props);
     console.log("Constructor");
   }
    
+  componentWillMount(){
+    console.log("componentWillMount");
+  }
+  
+  componentDidMount(){
+    console.log("componentDidMount");
+  }
+  
+  changeName =()=>{
+    this.setState({
+      name:"Ahmet"
+    });
+  }
+
    render(){
+     console.log("Render");
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Child name = {this.state.name}></Child>
+          <button onClick = {this.changeName} >Change Name</button>
         </header>
       </div>
     );
