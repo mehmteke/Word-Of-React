4f832f5977972cbc8bd3524daa723bf193a9cc14
Mehmet Teker <teker.mehmed@gmail.com>
2020-05-02 14:47:13 +0300

    Redirect ile Login örneği

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';   
-import {BrowserRouter as Router, Route, Link, NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, NavLink, Redirect} from 'react-router-dom';
 
 
 const news = ({match})=> {
     return(<h1>News Page : {match.params.id}</h1>)
 }
 
+const Profile = ()=>{
+  return(
+    <h1> Profile : Mehmet Teker</h1>
+  )
+}
+
 class App extends Component {
-  
+   
+  state = {
+    isLoading:false
+  }
+
+  onLogin = () => {
+     this.setState({
+       isLoading : !this.state.isLoading? true:false
+     })
+
+     console.log(this.state.isLoading);
+  }
+
   render(){ 
+    console.log(this.state.isLoading);
      return (
        <Router>
             <div>
                 <Link to="/">Home Page</Link> <br></br>
                 <NavLink activeStyle = {{color:'red'}} exact to="/Contact">Contact Page</NavLink> <br></br>
                 <NavLink activeClassName = "activeClassName" exact to="/News/2">News Page</NavLink> <br></br>
+                <NavLink activeClassName = "activeClassName" exact to="/Profile" >
+                  Profile Page
+                </NavLink> <br></br><br></br>
+                 <button type="button" value = { this.state.isLoading ? 'Logout' : 'Login'}
+                         onClick= {this.onLogin}> </button>
 
                 <Route path="/" exact render={
                                          () =>{ return (<h1>Home Page</h1>)}
@@ -25,6 +49,11 @@ class App extends Component {
                                                   ()=> { return (<h1>Contact Page</h1>)}
                                                 }/>
                 <Route path ='/News/:id' component = {news}/>
+                <Route path ='/Profile'  exect strict render = {
+                   () => { return( 
+                     this.state.isLoading ? <Profile></Profile> : <Redirect to = "/"></Redirect>
+                   )}
+                }/>
                 
             </div>
        </Router>
