7c1d7a8dfca25edd615b7b4a3c24e464377f945c
Mehmet Teker <teker.mehmed@gmail.com>
2020-05-02 13:08:25 +0300

    Route'ın component özelliğinin kullanılması

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
+const news = ()=> {
+  return(<h1>News Page</h1>)
+}
+
 class App extends Component {
   
   render(){ 
@@ -13,9 +17,10 @@ class App extends Component {
                 <Route path="/" exact render={
                                          () =>{ return (<h1>Home Page</h1>)}
                                         }/>
-                <Route path='/Contact' render= {
+                <Route path='/Contact' exact strict render= {
                                                   ()=> { return (<h1>Contact Page</h1>)}
                                                 }/>
+                <Route path ='/News' component = {news}/>
                 
             </div>
        </Router>
