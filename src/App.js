fbfd87f26c27afdbc92fa6236c038957ca3e880f
Mehmet Teker <teker.mehmed@gmail.com>
2020-05-02 18:00:07 +0300

    Redux' giriş. (npm install redux react-redux --save)

--- src/App.js
+++ src/App.js
@@ -1,66 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';   
-import {BrowserRouter as Router, Route, Link, NavLink, Redirect, Switch} from 'react-router-dom';
-import Error from './Error'
-
-const news = ({match})=> {
-    return(<h1>News Page : {match.params.id}</h1>)
-}
-
-const Profile = ()=>{
-  return(
-    <h1> Profile : Mehmet Teker</h1>
-  )
-}
+import './App.css';    
 
 class App extends Component {
    
-  state = {
-    isLoading:false
-  }
-
-  onLogin = () => {
-     this.setState({
-       isLoading : !this.state.isLoading? true:false
-     })
-
-     console.log(this.state.isLoading);
-  }
-
-  render(){ 
-    console.log(this.state.isLoading);
+  render(){  
      return (
-       <Router>
-            <div>
-                <Link to="/">Home Page</Link> <br></br>
-                <NavLink activeStyle = {{color:'red'}} exact to="/Contact">Contact Page</NavLink> <br></br>
-                <NavLink activeClassName = "activeClassName" exact to="/News/2">News Page</NavLink> <br></br>
-                <NavLink activeClassName = "activeClassName" exact to="/Profile" >
-                  Profile Page
-                </NavLink> <br></br><br></br>
-                 <button type="button" value = { this.state.isLoading ? 'Logout' : 'Login'}
-                         onClick= {this.onLogin}> </button>
-
-                <Switch>
-                     <Route path="/" exact render={
-                                              () =>{ return (<h1>Home Page</h1>)}
-                                             }/>
-                     <Route path='/Contact' exact strict render= {
-                                                       ()=> { return (<h1>Contact Page</h1>)}
-                                                     }/>
-                     <Route path ='/News/:id' component = {news}/>
-                     <Route path ='/Profile'  exact strict render = {
-                        () => { return( 
-                          this.state.isLoading ? <Profile></Profile> : <Redirect to = "/"></Redirect>
-                        )}
-                     }/>
-                     <Route exact component = {Error}></Route>
-                </Switch>
-                      
-                
-            </div>
-       </Router>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>React Redux Dersine HoşGeldiniz</h1>
+      </header>
+    </div>
     );
    }
 }
